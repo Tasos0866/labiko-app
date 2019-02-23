dec8d670a86b9e474dde5f752276b12b29cd874d
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-02-23 15:47:22 +0200

    translate: Added buttons for translation and set up of the translation process.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
-    BrowserModule, AgGridModule.withComponents([])
+    BrowserModule, HttpClientModule,
+    AgGridModule.withComponents([]),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  translate: TranslateService;
+
+  constructor(translate: TranslateService) {
+    this.translate = translate;
+    translate.setDefaultLang("en");
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 
   ngOnInit() {
   }
