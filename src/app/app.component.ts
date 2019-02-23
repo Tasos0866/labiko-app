a4f4845f13a5f0ec1514f7c0d8439e6a9fccbc0f
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-02-23 04:24:23 +0200

    homePage: The user can now input the month and its first day and that information is passed to home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'labiko-app';
+  day: string;
+  month: string
+  showGrid = false;
+  months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  days = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  nextClicked(day: string, month: string) {
+    if ((day != "") && (month != "")) {
+      this.day = day;
+      this.month = month;
+      this.showGrid = true;
+    }
+  }
 }
